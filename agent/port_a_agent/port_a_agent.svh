95c3d26917439b27bf163d7d2ed36686b0bd10c7
npcarter2025 <npcarter2025@gmail.com>
2026-01-25 15:59:10 -0800

    port_a_agent

---
+++ agent/port_a_agent/port_a_agent.svh
@@ -0,0 +1,34 @@
+`ifndef PORT_A_AGENT
+`define PORT_A_AGENT
+
+class port_a_agent extends uvm_agent;
+    `uvm_component_utils(port_a_agent)
+
+    port_a_driver drv;
+    port_a_monitor mon;
+    port_a_sequencer seqr;
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+
+        drv = port_a_driver::type_id::create("drv", this);
+        mon = port_a_monitor::type_id::create("mon", this);
+        seqr = port_a_sequencer::type_id::create("seqr", this);
+
+    endfunction
+
+    virtual function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+
+        drv.seq_item_port.connect(seqr.seq_item_export);
+    endfunction
+
+
+
+endclass
+
+`endif
